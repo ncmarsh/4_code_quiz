268d4b5edbeb5d31a749728194d97fbf069ea0ac
ncmarsh <68823943+ncmarsh@users.noreply.github.com>
2020-09-23 20:22:18 -0700

    added initial code for first question

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 var timerEl = document.querySelector("#timer");
-var mainEl = document.querySelector("#main");
+// var mainEl = document.querySelector("#main");
+var startEl = document.querySelector("#start");
+var questionsEl = document.querySelector("#questions");
 
 // Timer countdown function
-var secondsLeft = 75;
+var secondsLeft = 60;
 
 function setTime() {
     var timerInterval = setInterval(function() {
@@ -16,21 +18,56 @@ function setTime() {
     }, 1000);
 }
 
+// When start button is clicked, timer countdown starts 
+startEl.addEventListener("click", function() {
+    displayFirst();
+    setTime();
+})
+
+// Display first question 
+function displayFirst () {
+    console.log("first question");
+    var answerALiEl = document.createElement("li");
+    var answerBLiEl = document.createElement("li");
+    var answerCLiEl = document.createElement("li");
+    var answerDLiEl = document.createElement("li");
+    var answerOl = document.createElement("ol");
+
+    answerALiEl.setAttribute("class", "answer");
+    answerBLiEl.setAttribute("class", "answer");
+    answerCLiEl.setAttribute("class", "answer");
+    answerDLiEl.setAttribute("class", "answer");
+
+    answerOl.appendChild(answerALiEl);
+    answerOl.appendChild(answerBLiEl);
+    answerOl.appendChild(answerCLiEl);
+    answerOl.appendChild(answerDLiEl);
+    questionsEl.appendChild(answerOl);
+}
+
+document.addEventListener("click", function(event){
+    console.log(event.target);
+})
+
+window.onload = function () {
+    list = document.getElementsByClassName("answer");
+    for (var i = 0; i < list.length; i++) {
+      list[i].addEventListener("click", function (e) {
+          e.preventDefault();
+          console.log("clicked an answer");
+      });
+    }
+  };
+
 // Game over function
 function sendMessage() {
     timerEl.textContent = "GAME OVER";
-
-    // var imgEl = document.createElement("img");
-
-    // imgEl.setAttribute("src", "images/image_1.jpg");
-    // mainEl.appendChild(imgEl);
-  
+    var initialsText = prompt("GAME OVER! Please enter your initials");
+    console.log(initialsText);
 }
 
-setTime();
 
-// Initial welcome screen and start button
-// Timer needs to start when quiz start button is pushed
+
 // Cards need to shuffle through as questions are answered
 // Indicates after question answered about whether it was correct or wrong
 // time needs to subtract if a question is wrong ()=== false?)
